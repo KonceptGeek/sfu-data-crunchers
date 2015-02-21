4b48025a98bd87699bf7ee06071ac8042863cbf6
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2015-02-21 06:13:56 -0800

    Moved rent.json to misc, added relative paths, modified index.html to average rent for bachelor and 1 bedroom, ignoring 3 and 2 bedroom, rent only for year 2014

--- YouthEmployment/youth_employment/views.py
+++ YouthEmployment/youth_employment/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 import json
+import os
 
 
 def index(request):
+    currentDir = os.path.dirname(os.path.realpath(__file__))
+    joinedPath = os.path.join(currentDir,'../templates/assets/misc')
+    print joinedPath
+
     context = RequestContext(request)
-    with open('/Users/KonceptGeek/Documents/Projects/CODE2015/sfu-data-crunchers/YouthEmployment/youth_employment/rent.json') as rentFile:
+    with open(joinedPath+'/rent.json') as rentFile:
         rentJson = json.load(rentFile)
 
-    with open('/Users/KonceptGeek/Documents/Projects/CODE2015/sfu-data-crunchers/YouthEmployment/templates/assets/misc/canada.geojson') as geoFile:
+    with open(joinedPath+'/canada.geojson') as geoFile:
         geoJson = json.load(geoFile)
 
     rentJson = json.dumps(rentJson)
