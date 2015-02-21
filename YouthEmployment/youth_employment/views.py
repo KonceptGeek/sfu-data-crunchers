06320ec568ad46b248c41c6908f100b9a983ed4d
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2015-02-21 02:37:38 -0800

    starting with rent data

--- YouthEmployment/youth_employment/views.py
+++ YouthEmployment/youth_employment/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import json.read
 
 
 def index(request):
     context = RequestContext(request)
-    contextDict = {'testData': {'test': 'bla'}}
+    contextDict = {'testData': json.read(open("rent.json"))}
     return render_to_response('index.html', contextDict, context)
