3ba933146b1741f28c776e0ea37706182aff36c4
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2015-02-21 03:09:16 -0800

    added code to pass rent data and to read it in json with canada map

--- YouthEmployment/youth_employment/views.py
+++ YouthEmployment/youth_employment/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-import json.read
+import json
 
 
 def index(request):
     context = RequestContext(request)
-    contextDict = {'testData': json.read(open("rent.json"))}
+    with open('/Users/KonceptGeek/Documents/Projects/CODE2015/sfu-data-crunchers/YouthEmployment/youth_employment/rent.json') as rentFile:
+        rentJson = json.load(rentFile)
+    rentJson = json.dumps(rentJson)
+    contextDict = {'indexData': rentJson}
     return render_to_response('index.html', contextDict, context)
